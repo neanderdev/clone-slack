12e8858b3fb47fa6743838b9403634e22dd93f5f
Neander de Souza <neander.faria@hotmail.com>
2024-08-29 16:44:32 -0300

    Toolbar component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,16 @@
-interface WorkspaceIdPageProps {
-    params: {
-        workspaceId: string;
-    };
-};
+"use client";
 
-export default function WorkspaceIdPage({ params }: WorkspaceIdPageProps) {
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+export default function WorkspaceIdPage() {
+    const workspaceId = useWorkspaceId();
+    const { data } = useGetWorkspace({ id: workspaceId });
+
     return (
         <div>
-            ID: {params.workspaceId}
+            Data: {JSON.stringify(data)}
         </div>
     );
 }
