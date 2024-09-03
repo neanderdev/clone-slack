5c5ffbc8bdba8ded2247e4948df8b5110043900e
Neander de Souza <neander.faria@hotmail.com>
2024-09-03 15:21:44 -0300

    Invite modal

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -13,6 +13,7 @@ import {
 
 import { Doc } from "../../../../convex/_generated/dataModel";
 
+import { InviteModal } from "./invite-modal";
 import { PreferencesModal } from "./preferences-modal";
 
 interface WorkspaceHeaderProps {
@@ -22,9 +23,16 @@ interface WorkspaceHeaderProps {
 
 export function WorkspaceHeader({ workspace, isAdmin }: WorkspaceHeaderProps) {
     const [preferencesOpen, setPreferencesOpen] = useState(false);
+    const [inviteOpen, setInviteOpen] = useState(false);
 
     return (
         <>
+            <InviteModal
+                open={inviteOpen}
+                setOpen={setInviteOpen}
+                name={workspace.name}
+                joinCode={workspace.joinCode}
+            />
             <PreferencesModal
                 open={preferencesOpen}
                 setOpen={setPreferencesOpen}
@@ -68,7 +76,7 @@ export function WorkspaceHeader({ workspace, isAdmin }: WorkspaceHeaderProps) {
 
                                 <DropdownMenuItem
                                     className="cursor-pointer py-2"
-                                    onClick={() => { }}
+                                    onClick={() => setInviteOpen(true)}
                                 >
                                     Invite people to {workspace.name}
                                 </DropdownMenuItem>
